73402fcf925c8db5a7857a1d2574200af8c4ff18
288890428890404288904288904288904288904 <113226115+288890428890404288904288904288904288904@users.noreply.github.com>
2022-12-05 19:33:16 +0700

    Update BloxFruit.lua

--- BloxFruit.lua
+++ BloxFruit.lua
@@ -1583,7 +1583,7 @@ local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/hajib
 local Wait = library.subs.Wait
 
 local PepsisWorld = library:CreateWindow({
-    Name = "Alptra Hub",
+    Name = "Hyper Hub",
     Themeable = {
     Info = " "
     }
