cb93bb20b3c53f03bf878bdf1481e68fedc128f5
288890428890404288904288904288904288904 <113226115+288890428890404288904288904288904288904@users.noreply.github.com>
2022-12-16 21:58:00 +0700

    Update BloxFruit.lua

--- BloxFruit.lua
+++ BloxFruit.lua
@@ -149,7 +149,7 @@ _G.Settings = {
 	Auto_Store_Fruit = false;
 }
 
-local foldername = "Magma Hub Frre Script"
+local foldername = "Uria Hub Frre Script"
 local filename = "BloxFruit "..game.Players.LocalPlayer.Name.." Config.json"
  
 function saveSettings()
@@ -7530,7 +7530,7 @@ library.W = library.CreateWindow
 local Wait = library.subs.Wait
 
 local PepsisWorld = library:CreateWindow({
-    Name = "Magma Hub",
+    Name = "Uria Hub",
     Themeable = {
     }
 })
@@ -8599,6 +8599,88 @@ for i,v in pairs(getreg()) do
     end
 end
 
+SettingSection:AddToggle{
+    Name = "Supper Fast Attack",
+    Flag = "Supper_Fast",
+    Value = _G.Settings.Supper_Fast,
+    Callback  = function(value)
+        _G.Supper_Fast = value
+		_G.Settings.Supper_Fast = value
+		saveSettings()
+    end
+}
+
+
+local plr = game.Players.LocalPlayer
+    
+local CbFw = debug.getupvalues(require(plr.PlayerScripts.CombatFramework))
+local CbFw2 = CbFw[2]
+
+function GetCurrentBlade() 
+    local p13 = CbFw2.activeController
+    local ret = p13.blades[1]
+    if not ret then return end
+    while ret.Parent~=game.Players.LocalPlayer.Character do ret=ret.Parent end
+    return ret
+end
+function AttackNoCD() 
+    local AC = CbFw2.activeController
+    for i = 1, 1 do 
+        local bladehit = require(game.ReplicatedStorage.CombatFramework.RigLib).getBladeHits(
+            plr.Character,
+            {plr.Character.HumanoidRootPart},
+            60
+        )
+        local cac = {}
+        local hash = {}
+        for k, v in pairs(bladehit) do
+            if v.Parent:FindFirstChild("HumanoidRootPart") and not hash[v.Parent] then
+                table.insert(cac, v.Parent.HumanoidRootPart)
+                hash[v.Parent] = true
+            end
+        end
+        bladehit = cac
+        if #bladehit > 0 then
+            local u8 = debug.getupvalue(AC.attack, 5)
+            local u9 = debug.getupvalue(AC.attack, 6)
+            local u7 = debug.getupvalue(AC.attack, 4)
+            local u10 = debug.getupvalue(AC.attack, 7)
+            local u12 = (u8 * 798405 + u7 * 727595) % u9
+            local u13 = u7 * 798405
+            (function()
+                u12 = (u12 * u9 + u13) % 1099511627776
+                u8 = math.floor(u12 / u9)
+                u7 = u12 - u8 * u9
+            end)()
+            u10 = u10 + 1
+            debug.setupvalue(AC.attack, 5, u8)
+            debug.setupvalue(AC.attack, 6, u9)
+            debug.setupvalue(AC.attack, 4, u7)
+            debug.setupvalue(AC.attack, 7, u10)
+            pcall(function()
+                for k, v in pairs(AC.animator.anims.basic) do
+                    v:Play()
+                end                  
+            end)
+            if plr.Character:FindFirstChildOfClass("Tool") and AC.blades and AC.blades[1] then 
+                game:GetService("ReplicatedStorage").RigControllerEvent:FireServer("weaponChange",tostring(GetCurrentBlade()))
+                game.ReplicatedStorage.Remotes.Validator:FireServer(math.floor(u12 / 1099511627776 * 16777215), u10)
+                game:GetService("ReplicatedStorage").RigControllerEvent:FireServer("hit", bladehit, i, "") 
+            end
+        end
+    end
+end
+
+require(game.ReplicatedStorage.Util.CameraShaker):Stop()
+spawn(function()
+while task.wait() do
+      pcall(function()
+              if _G.Supper_Fast then
+                AttackNoCD() 
+              end
+      end)
+    end
+end)
 
 SettingSection:AddToggle{
     Name = "Auto Set Spawn",
